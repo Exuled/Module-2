7697a25514a88964b95d41de1bb9f4745acac1e4
Exuled <salanding@yandex.ru>
2024-07-16 22:51:45 +0900

    Add files via upload

---
+++ module_2_5.py
@@ -0,0 +1,18 @@
+def get_matrix(n, m, value):
+    if n <= 0 or m <= 0 or value <= 0:
+        return []
+    matrix = []
+    for i in range(n):
+        j = []
+        for i in range(m):
+            j.append(value)
+        matrix.append(j)
+    return matrix
+
+
+result1 = get_matrix(2, 2, 10)
+result2 = get_matrix(3, 5, 42)
+result3 = get_matrix(4, 2, 13)
+print(result1)
+print(result2)
+print(result3)
