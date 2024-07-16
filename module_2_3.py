6940f6cde3b2a420096153090cb8b347348c1835
Exuled <salanding@yandex.ru>
2024-07-16 18:15:52 +0900

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,11 @@
+my_list = [42, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+beg = 0
+while beg < len(my_list):
+    if my_list [beg] > 0:
+        print(my_list[beg])
+        beg += 1
+    elif my_list [beg] == 0:
+        beg += 1
+
+
+
